1bd89407fc4d0526c8116905c7f2c870419b2cac
fisenko-gb <fisenko-gb@yandex.ru>
2022-09-05 14:38:30 +0400

    05

--- Seminar01/homework01/05.py
+++ Seminar01/homework01/05.py
@@ -2,3 +2,13 @@
 # Пример:
 # A (3,6); B (2,1) -> 5,09
 # A (7,-5); B (1,-1) -> 7,21
+
+print('Введите координаты точки А:')
+x_A = float(input('X: '))
+y_A = float(input('Y: '))
+print("Введите координаты точки B:")
+x_B = float(input('X: '))
+y_B = float(input('Y: '))
+
+from math import sqrt
+print(f'A ({x_A}, {y_A}); B ({x_B}, {y_B}) -> ',round(sqrt((x_A - x_B)**2 + (y_A - y_B)**2), 2))  

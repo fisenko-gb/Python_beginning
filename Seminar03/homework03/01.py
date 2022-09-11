46a73f9003eb4b28e03acf0c9e262db9176fdc60
fisenko-gb <fisenko-gb@yandex.ru>
2022-09-11 17:44:33 +0400

    01

--- Seminar03/homework03/01.py
+++ Seminar03/homework03/01.py
@@ -3,3 +3,39 @@
 # Пример:
 
 # [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
+
+from random import randint
+
+
+def random_list(min: int, max: int, list_len: int) -> list:
+    '''
+    Фкнкция принимает на вход три параметра и возвращает список случайных числе
+    '''
+    return [randint(min, max) for i in range(list_len)]
+
+
+def sum_odd_index(t_list: list) -> int:
+    '''
+    Функция возращает словарь - sum - сумма чисел на не четных позициях, numbers - список значений элементов на не четных позициях
+    '''
+    slovar = {}
+    numbers = []
+    sum = 0
+    j = 0
+    while j <= len(t_list):
+        if j % 2 != 0:
+            sum += t_list[j]
+            numbers.append(t_list[j])
+        j += 1
+
+    slovar = slovar | {
+        "sum": sum,
+        "numbers": numbers
+    }
+    return slovar
+
+
+list_numbers = random_list(-10, 10, 10)
+rezult = sum_odd_index(list_numbers)
+print(
+    f'{list_numbers} -> на нечетных позициях элементы {rezult["numbers"]}, ответ: {rezult["sum"]}')

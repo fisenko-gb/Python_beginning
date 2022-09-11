e4f37842b7c7b242ab98fe9d0ec38b5ff7e04a09
fisenko-gb <fisenko-gb@yandex.ru>
2022-09-11 18:21:52 +0400

    02

--- Seminar03/homework03/02.py
+++ Seminar03/homework03/02.py
@@ -4,3 +4,33 @@
 
 # [2, 3, 4, 5, 6] = > [12, 15, 16]
 # [2, 3, 5, 6] = > [12, 15]
+from random import randint
+from tkinter.tix import TList
+
+
+def return_random_list(min: int, max: int, list_len: int) -> list:
+    '''
+    Фкнкция принимает на вход три параметра и возвращает список случайных числе
+    '''
+    return [randint(min, max) for i in range(list_len)]
+
+
+def multiplication_pairs(t_list: list) -> list:
+    '''
+    Функция возвращает произведение пар чисел списка
+    '''
+    i = 0
+    rezult = []
+    while i < len(t_list) / 2:
+        try:
+            rezult.append(t_list[i] * t_list[len(t_list) - (i + 1)])
+        except IndexError:
+            rezult.append(t_list[i] * t_list[i])
+        i += 1
+    return rezult
+
+
+numbers = return_random_list(-10, 10, randint(3, 10))
+rez = multiplication_pairs(numbers)
+
+print(f'{numbers} -> {rez}')

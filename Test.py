800ebf98e9e00a56de16f2310eabdd08dd06a5eb
fisenko-gb <fisenko-gb@yandex.ru>
2022-09-11 13:26:59 +0400

    fan_function

--- Test.py
+++ Test.py
@@ -1,8 +1,16 @@
 
-import time
-i = 100
-while i > 0:
-    print('%.9f' % time.time())
-    # print(time.monotonic_ns())
-    i -= 1
-    time.sleep(0.00001)
+import fan_function as fan
+
+# import time
+# i = 100
+# while i > 0:
+#     print('%.9f' % time.time())
+#     # print(time.monotonic_ns())
+#     i -= 1
+#     time.sleep(0.00001)
+
+i = 0
+while i < 10:
+    random_list = fan.random_list(1, 10, 10)
+    print(random_list)
+    i += 1
---
+++ fan_function.py
@@ -0,0 +1,62 @@
+from random import randint
+
+
+def random_list(min: int, max: int, list_len: int) -> list:
+    '''
+    Фкнкция принимает на вход три параметра и возвращает список случайных числе
+    '''
+    return [randint(min, max) for i in range(list_len)]
+
+
+def leave_only_numbers(st: str) -> str:
+    '''
+    Фкнкция принимает строку и возвращает строку только из цифр переданной строки
+    '''
+    rezult = ''
+
+    for i in st:
+        if ord(i) > ord('/') and ord(i) < ord(':'):
+            rezult += i
+
+    if st[0] == '-' and len(rezult) > 0:
+        rezult = '-' + rezult
+
+    return rezult
+
+
+def leave_only_char(st: str) -> str:
+    '''
+    Фкнкция принимает строку и возвращает строку БЕЗ цифр переданной строки
+    '''
+    rezult = ''
+
+    for i in st:
+        if (ord(i) >= 65 and ord(i) <= 90) or (ord(i) >= 97 and ord(i) <= 122) or (ord(i) >= 1040 and ord(i) <= 1103):
+            rezult += i
+
+    return rezult
+
+
+def random_my(min_number: int, max_number: int) -> int:
+    '''
+    Фкнкция принимает диапазон. в котором необходимо сгенирировать случайное число, на основании time, и возращает случайное число
+    '''
+
+    t_data = '%.9f' % time.time()
+    time.sleep(0.00001)
+    interval = max_number - min_number
+    razrad = len(str(interval)) * -1
+    smech = int(t_data[razrad:])
+
+    while smech > interval:
+        smech = int(smech / 2)
+
+    rnd = min_number + smech
+    return rnd
+
+
+def list_revers(x: list) -> list:
+    '''
+    Фкнкция принимает список и возращает его в перевернутом виде
+    '''
+    return x[::-1]

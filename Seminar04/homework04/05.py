994256cfd3688b4068ccb8a40705139aaf348762
fisenko-gb <fisenko-gb@yandex.ru>
2022-09-14 10:34:25 +0400

    HomeWork04

---
+++ Seminar04/homework04/05.py
@@ -0,0 +1,5 @@
+# 5 - Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных. Входные и выходные данные хранятся в отдельных текстовых файлах.
+# файл первый:
+# AAAAAAAAAAAABBBBBBBBBBBCCCCCCCCCCDDDDDDEEEEEFFFFG python is sooooooo coooooool
+# файл второй:
+# сжатый текст.

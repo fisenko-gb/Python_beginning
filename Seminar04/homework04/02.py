efc0484c77db5fa49b6a218ba6ba5a31c358d995
fisenko-gb <fisenko-gb@yandex.ru>
2022-09-14 10:47:01 +0400

    HomeWork04 02

--- Seminar04/homework04/02.py
+++ Seminar04/homework04/02.py
@@ -1,2 +1,16 @@
 # 2 - Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности. Не использовать множества.
 # [1,1,1,1,2,2,2,3,3,3,4] -> [1,2,3,4]
+
+def choose_different(t_list:list) -> list:
+    returt_list = []
+
+    for i in t_list:
+        if i not in returt_list:
+            returt_list.append(i)
+
+    return returt_list
+
+n_list = [1,1,1,1,2,2,2,3,3,3,4]
+print(f'{n_list} -> {choose_different(n_list)}')
+
+

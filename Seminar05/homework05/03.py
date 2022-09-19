bea7f493361dd5f463600c751ef002479d135daa
fisenko-gb <fisenko-gb@yandex.ru>
2022-09-19 19:00:06 +0400

    homework05 03

--- Seminar05/homework05/03.py
+++ Seminar05/homework05/03.py
@@ -13,3 +13,32 @@
 # Cложите получившиеся числа и верните из функции в качестве ответа вместе с преобразованным списком
 # https://dzen.ru/media/simplichka/kak-tekst-hranitsia-v-kompiutere-chast-3-62d3d91515d67a522f78e1e6?&
 
+def set_tuple(list1:list, list2:list) -> tuple:
+
+    for i in range(len(list1)):
+        list1[i] = list1[i].upper()
+
+
+    return dict(zip(list2, list1))
+
+def filter_elements(t_tuple:tuple):
+    new_tuple = {}
+    for key, value in t_tuple.items():
+        t_sum_kod = 0
+        for i in value:
+            t_sum_kod += ord(i)
+
+        proverka = t_sum_kod / key == int(t_sum_kod / key)
+        if proverka:
+            new_tuple[t_sum_kod] = value
+
+    return new_tuple
+
+
+language_list = ['Action Script', 'C++/CLI', 'C#', 'ColdFusion', 'Dart', 'Object Pascal', 'Dylan', 'Eiffel', 'Game Maker Language (GML)', 'Groovy', 'Haxe', 'Io', 'Java', 'JavaScript', 'MC#', 'Object Pascal']
+numbers_list = range(1, len(language_list) + 1)
+
+rez = (set_tuple(language_list, numbers_list))
+print(rez)
+print(filter_elements(rez))
+
